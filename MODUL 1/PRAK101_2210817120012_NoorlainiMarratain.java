41ec5ed205724a05326cbd6aa50d7aeb40f264e4
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2023-09-18 06:46:03 -0400

    Add files via upload

---
+++ MODUL 1/PRAK101_2210817120012_NoorlainiMarratain.java
@@ -0,0 +1,9 @@
+package MODUL1;
+
+public class PRAK101_2210817120012_NoorlainiMarratain {
+    public static void main(String[] args){
+        System.out.println("Selamat Pagi, Noorlaini Marratain");
+        System.out.println("Selamat Siang, Noorlaini Marratain");
+        System.out.println("Selamat Malam, Noorlaini Marratain");
+    }
+}
