e3df6066cd269768dce487b9adc85086358f5654
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2023-09-18 08:24:23 -0400

    Add files via upload

---
+++ MODUL 1/PRAK104_2210817120012_NoorlainiMarratain.java
@@ -0,0 +1,8 @@
+package MODUL1;
+public class PRAK104_2210817120012_NoorlainiMarratain {
+    public static void main(String[] args) {
+        String nama = "Nooralaini Marratain";
+        int umur = 19;
+        System.out.println("Perkenalkan, Nama Saya " + nama + ", Umur Saya " + umur);
+    }
+}
