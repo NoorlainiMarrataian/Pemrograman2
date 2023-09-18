dfb0886a3150b51f501d9ffaa289d24d8700a431
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2023-09-18 08:07:42 -0400

    Add files via upload

---
+++ MODUL 1/PRAK103_2210817120012_NoorlainiMarratain.java
@@ -0,0 +1,12 @@
+package MODUL1;
+public class PRAK103_2210817120012_NoorlainiMarratain {
+    public static void main(String[] args) {
+        System.out.println("#    #  #    #");
+        System.out.println("##   #  ##   #");
+        System.out.println("###  #  ###  #");
+        System.out.println("# ## #  # ## #");
+        System.out.println("#  ###  #  ###");
+        System.out.println("#   ##  #   ##");
+        System.out.println("#    #  #    #");
+    }
+}
