699a3278e4c057cace69ca82f4b40a5c8f9db4b3
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2023-11-20 19:57:51 -0500

    Add files via upload

---
+++ Modul 4/Soal1/Main.java
@@ -0,0 +1,17 @@
+package Soal1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inputan = new Scanner(System.in);
+        System.out.print("Nama Hewan Peliharaan: ");
+        String nama = inputan.nextLine();
+
+        System.out.print("Ras: ");
+        String ras = inputan.nextLine();
+
+        HewanPeliharaan DetilHewan = new HewanPeliharaan(ras, nama);
+        DetilHewan.display();
+    }
+}
